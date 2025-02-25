cc0d1e3da82872eb5d9cf7d15ed36bcc0619a5e9
Oscar Fernando Hoyos Carvajal <57969765+Viperexz@users.noreply.github.com>
2025-02-25 17:07:24 -0500

    Inicializacion del proyecto backend
    Se inicializa el proyecto backend con dos modulos.
    
    1. Gateway: Aplicacion encargada de redirigir el flujo de datos aun puerto especifico.
    
    2. Eureka: Aplicacion dedicada al descubrimiento de los servicios.
    
    Nota: Cada modulo debera tener el cliente de eureka.

---
+++ backend/tools/SchedulingConfig.java
@@ -0,0 +1,9 @@
+package com.nullco.rhservice.tools;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+}
