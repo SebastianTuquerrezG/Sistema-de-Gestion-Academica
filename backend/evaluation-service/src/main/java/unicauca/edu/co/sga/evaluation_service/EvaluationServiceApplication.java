791df05a740114767009c10ca35912151630f80d
Edwin Orlando <edwin415@outlook.com>
2025-03-21 10:04:56 -0500

    Fixed: Error of Embedding MariaDB
    It could be the fix of the error.
    New error is about Eureka.

--- backend/evaluation-service/src/main/java/unicauca/edu/co/sga/evaluation_service/EvaluationServiceApplication.java
+++ backend/evaluation-service/src/main/java/unicauca/edu/co/sga/evaluation_service/EvaluationServiceApplication.java
@@ -1,10 +1,16 @@
 package unicauca.edu.co.sga.evaluation_service;
 
+import org.springframework.ai.autoconfigure.vectorstore.mariadb.MariaDbStoreAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.Environment;
 
-@SpringBootApplication
+@SpringBootApplication(
+		exclude = {
+				// Maybe this could create a conflict with the data of the database
+				// because it's store of mariaDB, I think that.s
+				MariaDbStoreAutoConfiguration.class // With this you can exclude the error.
+		}
+)
 public class EvaluationServiceApplication {
 	public static void main(String[] args) { SpringApplication.run(EvaluationServiceApplication.class, args); }
 }
