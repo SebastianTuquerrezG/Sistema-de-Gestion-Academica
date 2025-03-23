4da61f93c160cefcbd3d74de8f96f6ece3654ab6
Everhech <edwin415@outlook.com>
2025-03-23 11:37:02 -0500

    Fixed: Connection with Eureka Server

--- backend/evaluation-service/src/main/java/unicauca/edu/co/sga/evaluation_service/EvaluationServiceApplication.java
+++ backend/evaluation-service/src/main/java/unicauca/edu/co/sga/evaluation_service/EvaluationServiceApplication.java
@@ -3,14 +3,16 @@ package unicauca.edu.co.sga.evaluation_service;
 import org.springframework.ai.autoconfigure.vectorstore.mariadb.MariaDbStoreAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(
 		exclude = {
 				// Maybe this could create a conflict with the data of the database
 				// because it's store of mariaDB, I think that.s
-				MariaDbStoreAutoConfiguration.class // With this you can exclude the error.
+				MariaDbStoreAutoConfiguration.class
 		}
 )
+@EnableDiscoveryClient
 public class EvaluationServiceApplication {
 	public static void main(String[] args) { SpringApplication.run(EvaluationServiceApplication.class, args); }
 }
