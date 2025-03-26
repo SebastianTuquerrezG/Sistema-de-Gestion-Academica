5eb656761645f7f4a4445e6234dbb628ceadf96e
Sebastian Tuquerrez <joangomezsebas@gmail.com>
2025-03-26 10:56:59 -0500

    Add interfaces ports to implement

--- backend/evaluation-service/src/main/java/unicauca/edu/co/sga/evaluation_service/application/ports/CriteriaPort.java
+++ backend/evaluation-service/src/main/java/unicauca/edu/co/sga/evaluation_service/application/ports/CriteriaPort.java
@@ -1,4 +1,16 @@
 package unicauca.edu.co.sga.evaluation_service.application.ports;
 
+import unicauca.edu.co.sga.evaluation_service.application.dto.request.CriteriaRequestDTO;
+import unicauca.edu.co.sga.evaluation_service.application.dto.response.CriteriaResponseDTO;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface CriteriaPort {
+    List<CriteriaResponseDTO> getCriteria();
+    Optional<CriteriaResponseDTO> getCriteriaById(Long id);
+    CriteriaResponseDTO saveCriteria(CriteriaRequestDTO criteria);
+    boolean deleteCriteria(Long id);
+    boolean updateCriteria(Long id, CriteriaRequestDTO criteria);
+    List<CriteriaResponseDTO> getCriteriaByPerformanceLevel(Long id);
 }
