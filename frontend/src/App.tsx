85134131f620182ab546a4426bbe228888902142
Pablo-Restrepo <restrepo736@gmail.com>
2025-03-27 01:57:50 -0500

    Add login page and related components; implement user validation schema

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import Dashboard from "./pages/Dashboard";
+import Login from "@/pages/Login";
 
 function App() {
   return (
--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -2,7 +2,6 @@ import { Routes, Route } from "react-router-dom";
 import DashboardLayout from "@/components/layouts/DashboardLayout";
 import CreateRubric from "@/components/rubrics/CreateRubric";
 import EditRubric from "@/components/rubrics/EditRubric";
-import RubricsList from "@/components/rubrics/RubricsList";
 import ConsultRubric from "@/components/rubrics/ConsultRubric";
 
 export default function Dashboard() {
@@ -10,7 +9,7 @@ export default function Dashboard() {
         <DashboardLayout>
             <Routes>
                 <Route index path="/rubricas" element={<ConsultRubric />} />
-                <Route path="/" element={<RubricsList />} />
+                <Route path="/" element={<ConsultRubric />} />
                 <Route path="/rubricas/crear" element={<CreateRubric />} />
                 <Route path="/rubricas/editar" element={<EditRubric />} />
             </Routes>
