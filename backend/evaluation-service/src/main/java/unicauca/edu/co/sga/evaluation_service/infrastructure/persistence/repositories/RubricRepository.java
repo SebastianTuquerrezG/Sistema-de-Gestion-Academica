b0a027da0e4a81c7908eb8d2ff030c2d97af3640
Jua-n10 <sebastianpisso00272@gmail.com>
2025-03-31 15:55:58 -0500

    Subiendo consulta rubrica descripcion

--- backend/evaluation-service/src/main/java/unicauca/edu/co/sga/evaluation_service/infrastructure/persistence/repositories/RubricRepository.java
+++ backend/evaluation-service/src/main/java/unicauca/edu/co/sga/evaluation_service/infrastructure/persistence/repositories/RubricRepository.java
@@ -1,6 +1,8 @@
 package unicauca.edu.co.sga.evaluation_service.infrastructure.persistence.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import unicauca.edu.co.sga.evaluation_service.domain.enums.GeneralEnums;
 import unicauca.edu.co.sga.evaluation_service.infrastructure.persistence.entities.RubricEntity;
@@ -15,5 +17,21 @@ public interface RubricRepository extends JpaRepository<RubricEntity, Long> {
     List<RubricEntity> findByRaId(Long id);
     //List<RubricEntity> findByCriteriaId(Long id);
     List<RubricEntity> findByStatus(GeneralEnums.status status);
+
+
+    @Query("SELECT r.study_objective FROM RubricEntity r " +
+            "JOIN r.subject sub " +
+            "JOIN sub.course c " +
+            "JOIN c.enroll e " +
+            "JOIN e.student s " +
+            "WHERE r.id = :rubricId " +
+            "AND sub.id = :subjectId " +
+            "AND s.id = :studentId " +
+            "AND e.semester = :semester")
+    Optional<String> findRubricDescriptionByStudent(
+            @Param("rubricId") Long rubricId,
+            @Param("subjectId") Long subjectId,
+            @Param("studentId") Long studentId,
+            @Param("semester") String semester);
 
 }
