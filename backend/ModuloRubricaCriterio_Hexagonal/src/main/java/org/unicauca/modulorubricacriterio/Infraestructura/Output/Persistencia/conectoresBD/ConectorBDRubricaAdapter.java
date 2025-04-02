ba142fd1d75cd11f027e9bf87d7ca329d638d9b0
Jeison Andrés Vallejo Gómez <jeisonvallejo@unicauca.edu.co>
2025-04-02 16:14:28 -0500

    HexagonalTerminado
    Hexagonal Terminado separando lógica del negocio de la infraestructura, se adjuntan todas las pruebas postman

---
+++ backend/ModuloRubricaCriterio_Hexagonal/src/main/java/org/unicauca/modulorubricacriterio/Infraestructura/Output/Persistencia/conectoresBD/ConectorBDRubricaAdapter.java
@@ -0,0 +1,173 @@
+package org.unicauca.modulorubricacriterio.Infraestructura.Output.Persistencia.conectoresBD;
+
+import java.util.List;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.stereotype.Service;
+import org.unicauca.modulorubricacriterio.Aplicación.Output.IConectorBDRubricaPort;
+
+import org.unicauca.modulorubricacriterio.Dominio.Modelos.Rubrica;
+import org.unicauca.modulorubricacriterio.Infraestructura.Fachada.exception.exceptionOwn.CambioInvalidoException;
+import org.unicauca.modulorubricacriterio.Infraestructura.Fachada.exception.exceptionOwn.EntidadNoExisteException;
+import org.unicauca.modulorubricacriterio.Infraestructura.Input.validacionEstados.EstadosController;
+import org.unicauca.modulorubricacriterio.Infraestructura.Input.validacionEstados.EstadosEnum;
+import org.unicauca.modulorubricacriterio.Infraestructura.Output.Persistencia.entity.RubricaEntity;
+import org.unicauca.modulorubricacriterio.Infraestructura.Output.Persistencia.repository.RubricaRepository;
+
+@Service
+public class ConectorBDRubricaAdapter implements IConectorBDRubricaPort {
+
+    private final RubricaRepository rubricaRepository;
+    private final ModelMapper rubricaMapper;
+
+    public ConectorBDRubricaAdapter(RubricaRepository rubricaRepository, ModelMapper rubricaMapper) {
+        this.rubricaRepository = rubricaRepository;
+        this.rubricaMapper = rubricaMapper;
+    }
+
+    @Override
+    public List<Rubrica> findAll() {
+        List<RubricaEntity> listRubricaEntity = this.rubricaRepository.findAll();
+        List<Rubrica> listRubrica = this.rubricaMapper.map(listRubricaEntity, new TypeToken<List<Rubrica>>() {
+        }.getType());
+        for (Rubrica rubric : listRubrica) {
+            if(rubric.getCriterios() != null)
+            {
+                rubric.getCriterios().forEach(criterio -> {
+                    criterio.setIdRubrica(rubric.getIdRubrica());
+                    if(criterio.getNiveles() != null) {
+                        criterio.getNiveles().forEach(nivel -> nivel.setIdCriterio(criterio.getIdCriterio()));
+                    }
+                });
+            }
+        }
+        return listRubrica;
+    }
+
+    @Override
+    public Rubrica findById(Long idRubrica) {
+        RubricaEntity objRubricaEntity = this.rubricaRepository.findById(idRubrica).orElse(null);
+
+        if (objRubricaEntity == null) {
+            return null;
+        }
+
+        Rubrica objRubrica = this.rubricaMapper.map(objRubricaEntity, Rubrica.class);
+        if(objRubrica.getCriterios()!= null)
+        {
+            objRubrica.getCriterios().forEach(criterio -> {
+                criterio.setIdRubrica(objRubrica.getIdRubrica());
+                if(criterio.getNiveles() != null) {
+                    criterio.getNiveles().forEach(nivel -> nivel.setIdCriterio(criterio.getIdCriterio()));
+                }
+            });
+
+        }
+        
+        return objRubrica;
+    }
+
+    @Override
+    public Rubrica saveRubric(Rubrica objRubrica) {
+        RubricaEntity objRubricaEntity = this.rubricaMapper.map(objRubrica, RubricaEntity.class);
+        RubricaEntity objRubricaEntityGuardada = this.rubricaRepository.save(objRubricaEntity);
+        Rubrica objRubricaGuardada = this.rubricaMapper.map(objRubricaEntityGuardada, Rubrica.class);
+        return objRubricaGuardada;
+    }
+
+    @Override
+    public Rubrica updateRubric(Long id, Rubrica objRubrica) {
+        RubricaEntity objRubricaActualizada = null;
+        RubricaEntity rubricaEncontrada = this.rubricaRepository.findById(id).orElse(null);
+        if(rubricaEncontrada==null)
+        {
+            throw new EntidadNoExisteException("Rúbrica con el id {"+id+"} no existe");
+        }
+        rubricaEncontrada.setMateria(objRubrica.getMateria());
+        rubricaEncontrada.setNombreRubrica(objRubrica.getNombreRubrica());
+        rubricaEncontrada.setNotaRubrica(objRubrica.getNotaRubrica());
+        rubricaEncontrada.setObjetivoEstudio(objRubrica.getObjetivoEstudio());
+        objRubricaActualizada = this.rubricaRepository.save(rubricaEncontrada);
+        Rubrica rubricaActualizadaReturn = this.rubricaMapper.map(objRubricaActualizada, Rubrica.class);
+        
+        if(rubricaActualizadaReturn.getCriterios() != null)
+        {
+            rubricaActualizadaReturn.getCriterios().forEach(criterio -> {
+                criterio.setIdRubrica(objRubrica.getIdRubrica());
+                if(criterio.getNiveles() != null) {
+                    criterio.getNiveles().forEach(nivel -> nivel.setIdCriterio(criterio.getIdCriterio()));
+                }
+            });
+        }
+
+        return rubricaActualizadaReturn;   
+    }
+
+    @Override
+    public Rubrica deleteRubric(Long IdRubrica) {
+        RubricaEntity objRubricaAEliminar = this.rubricaRepository.findById(IdRubrica).orElse(null);
+        if(objRubricaAEliminar == null)
+        {
+            throw new EntidadNoExisteException("Rúbrica con el id {"+IdRubrica+"} no existe");
+        }
+        Rubrica objRubrica = this.rubricaMapper.map(objRubricaAEliminar, Rubrica.class);
+        this.rubricaRepository.deleteById(IdRubrica);
+        return objRubrica;
+    }
+
+    @Override
+    public Rubrica changeEstate(Long idRubrica, String estadoRubrica) {
+        RubricaEntity objRubricaActualizada = null;
+        EstadosEnum estadoSiguiente = validarEstado(estadoRubrica);
+        RubricaEntity rubricaEncontrada = this.rubricaRepository.findById(idRubrica).orElse(null);
+        if(rubricaEncontrada==null)
+        {
+            throw new EntidadNoExisteException("Rúbrica con el id {"+idRubrica+"} no existe");
+        }
+
+        EstadosController objEstadoActual  = new EstadosController(rubricaEncontrada.getEstadoRubrica().ordinal());
+
+        if(objEstadoActual.cambioEstado(estadoRubrica).cambioPermitido())
+        {
+            rubricaEncontrada.setEstadoRubrica(estadoSiguiente);
+        }else
+        {
+            throw new CambioInvalidoException(objEstadoActual.cambioEstado(estadoRubrica).mensaje());
+        }
+
+        objRubricaActualizada = this.rubricaRepository.save(rubricaEncontrada);
+
+        Rubrica rubricaRetornar = this.rubricaMapper.map(objRubricaActualizada, Rubrica.class);
+        
+        if(rubricaRetornar.getCriterios() != null)
+        {
+            rubricaRetornar.getCriterios().forEach(criterio -> {
+                criterio.setIdRubrica(rubricaRetornar.getIdRubrica());
+                if(criterio.getNiveles() != null) {
+                    criterio.getNiveles().forEach(nivel -> nivel.setIdCriterio(criterio.getIdCriterio()));
+                }
+            });
+        }
+        
+
+        return rubricaRetornar;
+    }
+
+    private EstadosEnum validarEstado(String Estado)
+    {
+        EstadosEnum estadoSiguiente = null;
+        switch (Estado) {
+            case "ACTIVAR":
+                estadoSiguiente = EstadosEnum.ACTIVO;
+                break;
+            case "DESACTIVAR":
+                estadoSiguiente = EstadosEnum.INACTIVO;
+                break;
+        }
+        return estadoSiguiente;
+    }
+
+
+
+}
