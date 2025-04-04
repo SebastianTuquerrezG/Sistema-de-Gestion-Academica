f40b9e89fb1e0d7ec4113bf79c25e2e4cc253bed
Oscar Fernando Hoyos Carvajal <57969765+Viperexz@users.noreply.github.com>
2025-04-04 00:58:08 -0500

    .

---
+++ backend/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Iniciar erueka-server con perfil prod
+tmux new-session -d -s eureka-server
+tmux send-keys -t eureka-server:0 './environment.sh' C-m 'cd eureka-server' C-m 'mvn spring-boot:run' C-m
+
+# Iniciar gateway con perfil prod
+tmux new-session -d -s gateway
+tmux send-keys -t gateway:0 './environment.sh' C-m 'cd gateway' C-m 'mvn spring-boot:run' C-m
+
+# Iniciar auth-service con perfil prod
+tmux new-session -d -s evaluation-service
+tmux send-keys -t auth-service:0 './environment.sh' C-m 'cd evaluation-service' C-m 'mvn spring-boot:run' C-m
+
+# Iniciar audit-service con perfil prod
+tmux new-session -d -s ModuloRubricaCriterio_Hexagonal-service
+tmux send-keys -t audit-service:0 './environment.sh' C-m 'cd ModuloRubricaCriterio_Hexagonal' C-m 'mvn spring-boot:run' C-m
+
