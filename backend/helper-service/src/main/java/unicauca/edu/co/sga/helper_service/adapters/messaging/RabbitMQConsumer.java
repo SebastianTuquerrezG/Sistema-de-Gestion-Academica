075fe14dc37689cbd7a691c4069f675d1a56811f
Edwin Orlando <edwin415@outlook.com>
2025-05-13 08:32:14 -0500

    feature: new implementation of rabbit

--- backend/helper-service/src/main/java/unicauca/edu/co/sga/helper_service/adapters/messaging/RabbitMQConsumer.java
+++ backend/helper-service/src/main/java/unicauca/edu/co/sga/helper_service/adapters/messaging/RabbitMQConsumer.java
@@ -8,18 +8,30 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 import unicauca.edu.co.sga.helper_service.application.dto.request.EnrollRequestDTO;
+import unicauca.edu.co.sga.helper_service.application.dto.response.EnrollResponseDTO;
 import unicauca.edu.co.sga.helper_service.infrastructure.config.RabbitMQConfig;
 import unicauca.edu.co.sga.helper_service.infrastructure.persistence.entities.EnrollEntity;
 import unicauca.edu.co.sga.helper_service.infrastructure.persistence.entities.EvaluationEntity;
+import unicauca.edu.co.sga.helper_service.infrastructure.persistence.mappers.EnrollMapper;
+import unicauca.edu.co.sga.helper_service.infrastructure.persistence.repositories.EnrollRepository;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class RabbitMQConsumer {
+    private final EnrollMapper enrollMapper;
+    private final EnrollRepository enrollRepository;
+
     @RabbitListener(queues = RabbitMQConfig.QUEUE_ENROLL)
     public void readEnrollData(@Payload EnrollRequestDTO message) {
-//        EnrollEntity entity = new ObjectMapper().readValue(message, EnrollEntity.class);
+        EnrollEntity enrollEntity = enrollMapper.toEntity(enrollMapper.toModel(message));
         log.info("Message from EVALUATION microservice (ENROLL ENTITY): {}", message);
+        EnrollResponseDTO enrollResponseDTO = enrollMapper.toDTO(enrollMapper.toModel(enrollRepository.save(enrollEntity)));
+        if(enrollResponseDTO != null){
+            log.info("Database updated with new enroll data. Data {}", enrollResponseDTO);
+        }else{
+            log.info("There are some errors with the database.");
+        }
     }
 
     @RabbitListener(queues = RabbitMQConfig.QUEUE_EVALUATION)
