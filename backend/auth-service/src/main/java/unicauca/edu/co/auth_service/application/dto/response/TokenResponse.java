fa7b9e25e02bd774d5d3b0367757daf5655315e3
Joan Sebastian Tuquerrez Gomez <jtuquerrez@unicauca.edu.co>
2025-05-17 18:53:04 -0500

    Add auth service

---
+++ backend/auth-service/src/main/java/unicauca/edu/co/auth_service/application/dto/response/TokenResponse.java
@@ -0,0 +1,24 @@
+package unicauca.edu.co.auth_service.application.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TokenResponse {
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+
+    @JsonProperty("expires_in")
+    private Long expiresIn;
+}
