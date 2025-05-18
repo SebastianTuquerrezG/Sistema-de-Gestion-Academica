9a32f67f68901a2ee84a541fb34190523896926f
Joan Sebastian Tuquerrez Gomez <jtuquerrez@unicauca.edu.co>
2025-05-17 19:18:38 -0500

    Modify dockerfiles, preparing to deploy all services, next to do fix profiles docker and local

--- backend/build.sh
+++ backend/build.sh
@@ -2,11 +2,15 @@
 
 # Iniciar erueka-server con perfil prod
 tmux new-session -d -s eureka-server
-tmux send-keys -t eureka-server:0 './environment.sh' C-m 'cd eureka-server' C-m 'mvn spring-boot:run' C-m
+tmux send-keys -t eureka-server:0 './environment.sh' C-m 'cd eureka-server' C-m 'mvn spring-boot:run -DskipTests' C-m
 
 # Iniciar gateway con perfil prod
 tmux new-session -d -s gateway
-tmux send-keys -t gateway:0 './environment.sh' C-m 'cd gateway' C-m 'mvn spring-boot:run' C-m
+tmux send-keys -t gateway:0 './environment.sh' C-m 'cd gateway' C-m 'mvn spring-boot:run -DskipTests' C-m
+
+# Iniciar auth-service con perfil prod
+tmux new-session -d -s auth-service
+tmux send-keys -t auth-service:0 './environment.sh' C-m 'cd auth-service' C-m 'mvn spring-boot:run -DskipTests' C-m
 
 # Iniciar evaluation-service con perfil prod
 tmux new-session -d -s evaluation-service
@@ -14,8 +18,8 @@ tmux send-keys -t evaluation-service:0 './environment.sh' C-m 'cd evaluation-ser
 
 # Iniciar rubric-service con perfil prod
 tmux new-session -d -s ModuloRubricaCriterio_Hexagonal-service
-tmux send-keys -t ModuloRubricaCriterio_Hexagonal-serv:0 './environment.sh' C-m 'cd ModuloRubricaCriterio_Hexagonal' C-m 'mvn spring-boot:run' C-m
+tmux send-keys -t ModuloRubricaCriterio_Hexagonal-serv:0 './environment.sh' C-m 'cd ModuloRubricaCriterio_Hexagonal' C-m 'mvn spring-boot:run -DskipTests' C-m
 
 # Iniciar common-service con perfil prod
-tmux new-session -d -s helper-service
-tmux send-keys -t helper-serv:0 './environment.sh' C-m 'cd helper-service' C-m 'mvn spring-boot:run -DskipTests' C-m
+tmux new-session -d -s common_utilities-service
+tmux send-keys -t common_utilities-service:0 './environment.sh' C-m 'cd common_utilities-service' C-m 'mvn spring-boot:run -DskipTests' C-m
